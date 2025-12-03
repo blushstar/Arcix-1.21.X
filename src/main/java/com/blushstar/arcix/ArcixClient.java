af3f5a8e468bf078179d7b2f273894da61f09d1f
blushstar <evie@stingray.zone>
2025-12-03 19:41:58 +0100

    Initial

---
+++ src/main/java/com/blushstar/arcix/ArcixClient.java
@@ -0,0 +1,10 @@
+package com.blushstar.arcix;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArcixClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/blushstar/arcix/ArcixDataGenerator.java
@@ -0,0 +1,11 @@
+package com.blushstar.arcix;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ArcixDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
